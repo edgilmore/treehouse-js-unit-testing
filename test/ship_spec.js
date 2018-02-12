3fb0a425ab3b884d836761c4041a503984c5d94f
Edward Gilmore <edgilmore@live.com>
2018-02-12 16:52:45 -0700

    added a spec for the ship logic testing

---
+++ test/ship_spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai');
+
+describe('checkForShip', () => {
+  const {checkForShip} = require('../game/logic/ship_methods');
+  it('should correctly report no ship at a given players coordinate', (coordinate, player) =>{
+
+  });
+});
