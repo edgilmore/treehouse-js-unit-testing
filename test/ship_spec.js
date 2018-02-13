01bc9058f04ffcc642db513430eebd1a51be3315
Edward Gilmore <edgilmore@live.com>
2018-02-13 08:31:15 -0700

    added a test for the checkForShip function

--- test/ship_spec.js
+++ test/ship_spec.js
@@ -1,8 +1,8 @@
-const expect = require('chai');
+const expect = require("chai");
 
-describe('checkForShip', () => {
-  const {checkForShip} = require('../game/logic/ship_methods');
-  it('should correctly report no ship at a given players coordinate', (coordinate, player) =>{
-
+describe("checkForShip", () => {
+  const { checkForShip } = require("../game/logic/ship_methods");
+  it("should correctly report no ship at a given players coordinate", () => {
+    expect(checkForShip(player, [9, 9]));
   });
 });
