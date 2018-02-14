1509a46029725e1014e78aa635513e8c60e77750
Edward Gilmore <edgilmore@live.com>
2018-02-14 14:03:39 -0700

    implemented checkForShip function

--- game/logic/ship_methods.js
+++ game/logic/ship_methods.js
@@ -1,3 +1,14 @@
-function checkForShip(playerId, coordinatesArray) {}
+function checkForShip(player, coordinates) {
+  let shipPresent;
+  let ship;
+
+  for (let i = 0; i < player.ships.length; i += 1) {
+    ship = player.ships[i];
+    shipPresent = ship.locations.filter(
+      location => location[0] === coordinates[0] && location[1] === coordinates[1],
+    );
+  }
+  return shipPresent.length !== 0;
+}
 
 module.exports.checkForShip = checkForShip;
