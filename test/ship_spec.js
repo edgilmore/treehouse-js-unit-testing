77b1e7b24c7fe3a8af70da0604bc7e9a72a52f4b
Edward Gilmore <edgilmore@live.com>
2018-02-14 15:17:10 -0700

    added additional tests for multiple ship scenarios

--- test/ship_spec.js
+++ test/ship_spec.js
@@ -1,12 +1,39 @@
+/* eslint-env mocha */
 const { expect } = require('chai');
-const { describe, it } = require('mocha');
-const { checkForShip } = require('../game/logic/ship_methods');
+const { checkForShip, damageShip } = require('../game/logic/ship_methods');
 
 describe('checkForShip', () => {
+  const player = {
+    ships: [
+      { locations: [[0, 0], [0, 1], [9, 9]] },
+      { locations: [[0, 1], [0, 0], [1, 2]] },
+      { locations: [[0, 6], [1, 5], [9, 5]] },
+    ],
+  };
   it('should correctly report no ship at a given players coordinate', () => {
-    const player = {
-      ships: [{ locations: [[0, 0]] }],
-    };
-    expect(checkForShip(player, [9, 9]));
+    // eslint-disable-next-line
+    expect(checkForShip(player, [1, 1])).to.be.false;
+  });
+  it('should correctly report a ship present at given players coordiante', () => {
+    // eslint-disable-next-line
+    expect(checkForShip(player, [9, 9])).to.be.true;
+  });
+  it('should handle ships located at more than one coordinate', () => {
+    // eslint-disable-next-line
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    // eslint-disable-next-line
+    expect(checkForShip(player, [2, 2])).to.be.false;
+  });
+  it('should handle checking multiple ships', () => {
+    // eslint-disable-next-line
+    expect(checkForShip(player, [1, 2])).to.be.true;
+    // eslint-disable-next-line
+    expect(checkForShip(player, [2, 2])).to.be.false;
+    // eslint-disable-next-line
+    expect(checkForShip(player, [0, 0])).to.be.true;
   });
 });
+
+describe('damageShip', () => {
+
+})
