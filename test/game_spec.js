a1b197d02a21ad19d380585c2305c031838fc971
Edward Gilmore <edgilmore@live.com>
2018-02-15 16:47:36 -0700

    added some pending tests

---
+++ test/game_spec.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+const { expect } = require('chai');
+
+describe('GAME INSTANCE FUNCTIONS', () => {
+  describe('checkGameStatus', () => {
+    it('should tell me when the game is over');
+  });
+  xit('some other stuff', () => {});
+});
