acc3ce7fac55eabbed1e3aa5e00d3a3260cfc136
Edward Gilmore <edgilmore@live.com>
2018-02-15 17:13:11 -0700

    added the implementation for the takeTurn function in the game logic

--- game/logic/game_instance.js
+++ game/logic/game_instance.js
@@ -1,3 +1,15 @@
-function checkGameStatus(players) {}
+const { fire } = require('./ship_methods');
+
+function checkGameStatus(players) {
+  return false;
+}
+
+function takeTurn(opposingPlayer, guessFunction) {
+  const coordinates = guessFunction();
+  fire(opposingPlayer, coordinates);
+  const gameOver = checkGameStatus();
+  return gameOver;
+}
 
 module.exports.checkGameStatus = checkGameStatus;
+module.exports.takeTurn = takeTurn;
