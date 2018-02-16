89b4f13a2cbc7240a4789da5b641ecd8ee14e138
Edward Gilmore <edgilmore@live.com>
2018-02-15 17:12:46 -0700

    added a test for the takeTurn function in the game instance

--- test/game_spec.js
+++ test/game_spec.js
@@ -1,9 +1,30 @@
 /* eslint-env mocha */
 const { expect } = require('chai');
+const { takeTurn, checkGameStatus } = require('../game/logic/game_instance');
 
 describe('GAME INSTANCE FUNCTIONS', () => {
   describe('checkGameStatus', () => {
     it('should tell me when the game is over');
+  });
+  describe('takeTurn', () => {
+    let guess;
+    let player;
+    beforeEach(() => {
+      guess = () => [0, 0];
+      player = {
+        ships: [
+          {
+            locations: [[0, 0]],
+            damage: [],
+          },
+        ],
+      };
+    });
+    it('should return false if the game ends', () => {
+      const actual = takeTurn(player, guess);
+      // eslint-disable-next-line
+      expect(actual).to.be.false;
+    });
   });
   xit('some other stuff', () => {});
 });
